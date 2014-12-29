4a412dbb1b06ffd4d5c68f73b97d90e07b18984f
danello02 <danello02b@gmail.com>
2014-12-29 18:36:17 +0100

    Create class BasicEdge

---
+++ PackageOfGeneratorsPlugin/src/pl/edu/wat/dbocian/et/plugin/data/BasicEdge.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2008-2010 Gephi
+ * Authors : Daniel Bocian
+ * Website : http://www.gephi.org
+ * 
+ * This file is part of Gephi.
+ *
+ * Gephi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gephi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gephi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package pl.edu.wat.dbocian.et.plugin.data;
+
+/**
+ * @author Daniel
+ */
+public class BasicEdge {
+
+    private int sourceId;
+    private int targetId;
+
+    public BasicEdge() {
+    }
+
+    public BasicEdge(int sourceId, int targetId) {
+        this.sourceId = sourceId;
+        this.targetId = targetId;
+    }
+
+    public int getSourceId() {
+        return sourceId;
+    }
+
+    public void setSourceId(int sourceId) {
+        this.sourceId = sourceId;
+    }
+
+    public int getTargetId() {
+        return targetId;
+    }
+
+    public void setTargetId(int targetId) {
+        this.targetId = targetId;
+    }
+}
